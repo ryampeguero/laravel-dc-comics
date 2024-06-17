d0243eaa1558aea3d7588e9f5e45e852751301ce
ryampeguero <ryampeguero@gmail.com>
2024-06-17 22:59:43 +0200

    feat: Refactoring of javascript code

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,21 +3,25 @@ import "~resources/scss/app.scss";
 import * as bootstrap from "bootstrap";
 import.meta.glob(["../img/**"]);
 
-const deleteButtons = document.querySelectorAll('.prova');
-
-// console.log(deleteButtons);
+//Prendo dal dom tutti gli elementi con id formDelete
 const deleteForm = document.querySelectorAll('#formDelete');
 
 // console.log(deleteForm);
-
+//Aggiungo ad ognuno degli elementi un eventListenr
 deleteForm.forEach((currForm, index) => {
-    console.log(currForm);
+    // console.log(currForm);
 
     currForm.addEventListener('submit', (event) => {
-        event.preventDefault(); //Blocca il form dal submit
+        //Blocca il form dal submit
+        event.preventDefault(); 
+        
+        //Prendo l'elemento del modal con id delete
         const deleteModalButton = document.querySelector('#delete');
-        console.log(deleteModalButton);
+        // console.log(deleteModalButton);
+
+        //Aggiungo eventListener
         deleteModalButton.addEventListener('click', () => {
+            //Faccio il submit del form corrente selezionato
             currForm.submit();
         });
     });
